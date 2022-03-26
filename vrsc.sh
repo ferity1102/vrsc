6ea9dff9ea7367a95c3b6b76e193385a1dacf2c4
ferity1102 <92764631+ferity1102@users.noreply.github.com>
2022-03-26 20:40:04 +0700

    Update vrsc.sh

--- vrsc.sh
+++ vrsc.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u wallet.worke -p x --cpu 2
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RAtp3eqwa4eQe1oQh51udKbqekiEhoLjVR -p c=VRSC
 while [ 1 ]; do
 sleep 3
 done

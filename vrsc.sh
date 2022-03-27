840ec6dcdddfcb91c0d4bf2943f7d6e028ed97e9
ferity1102 <92764631+ferity1102@users.noreply.github.com>
2022-03-27 10:29:28 +0700

    Update vrsc.sh

--- vrsc.sh
+++ vrsc.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RAtp3eqwa4eQe1oQh51udKbqekiEhoLjVR -p c=VRSC
+git clone https://github.com/hmage/cpuminer-opt
+cd cpuminer-opt
+sudo apt-get install libssl-dev libcurl4-openssl-dev libjansson-dev
+./build.sh
+./cpuminer -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RAtp3eqwa4eQe1oQh51udKbqekiEhoLjVR -p c=VRSC
 while [ 1 ]; do
 sleep 3
 done

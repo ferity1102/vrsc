9cdb2b38ffa7d8229b9dc0a22688addf7f293761
ferity1102 <92764631+ferity1102@users.noreply.github.com>
2022-03-27 10:35:51 +0700

    Update vrsc.sh

--- vrsc.sh
+++ vrsc.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-git clone https://github.com/hmage/cpuminer-opt
-cd cpuminer-opt
-sudo apt-get install libssl-dev libcurl4-openssl-dev libjansson-dev
-sh build.sh
+wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.19.6.tar.gz
+tar xvzf cpuminer-opt-3.19.6.tar.gz
+./build.sh
 ./cpuminer -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RAtp3eqwa4eQe1oQh51udKbqekiEhoLjVR -p c=VRSC
 while [ 1 ]; do
 sleep 3

